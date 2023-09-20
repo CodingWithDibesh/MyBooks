dceffadeeb55ec6ec739dd79e5e64d9633c463fb
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2023-09-20 09:35:25 +0545

    refactor: Implemented Barrel Export

--- helper/index.ts
+++ helper/index.ts
@@ -1,33 +1,3 @@
-export interface IResponse<T> {
-	success: boolean;
-	status: number;
-	length?: number;
-	data: T[];
-}
-
-export const replacePublic = (item: string, url: string) => {
-	return item.replaceAll("/public/", url);
-};
-
-export const responseHandler = (response: any, statusCode: number = 200) => {
-	return response.length > 1
-		? {
-				success: statusCode < 300 && statusCode >= 200,
-				status: statusCode,
-				length: response.length,
-				data: response,
-		  }
-		: {
-				success: statusCode < 300 && statusCode >= 200,
-				status: statusCode,
-				data: response,
-		  };
-};
-
-export const lenSlice = (str: string, len: number = 210): string =>
-	str.slice(0, len) + "...";
-
-export const isObjEmpty = (obj: Object): boolean =>
-	obj &&
-	Object.keys(obj).length === 0 &&
-	Object.getPrototypeOf(obj) === Object.prototype;
+export * from "./pwa.helper"
+export * from "./response.helper"
+export * from "./string.helper"
